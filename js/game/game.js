2122d458dd1cfe9948a987e2c8df26b8f7f685e1
Your Name <your@example.com>
2015-07-14 09:52:57 -0500

    .

--- js/game/game.js
+++ js/game/game.js
@@ -5,7 +5,10 @@
 	}
 
 	SDD.Game.prototype.preload = function() {
-		var gameSystem = this.gameSystem;
+		this.gameSystem.loadImage('space', 'assets/deep-space.jpg');
+		this.gameSystem.loadImage('bullet', 'assets/bullets.png');
+		this.gameSystem.loadImage('ship', 'assets/ship.png');
+		this.gameSystem.loadImage('crosshair', 'assets/green_Crosshair.png');
 	};
 
 	SDD.Game.prototype.create = function() {
--- js/game/init.js
+++ js/game/init.js
@@ -6,10 +6,7 @@
 	}
 
 	SDD.Init.prototype.preload = function() {
-		this.gameSystem.loadImage('space', 'assets/deep-space.jpg');
-		this.gameSystem.loadImage('bullet', 'assets/bullets.png');
-		this.gameSystem.loadImage('ship', 'assets/ship.png');
-		this.gameSystem.loadImage('crosshair', 'assets/green_Crosshair.png');
+		
 	};
 
 	SDD.Init.prototype.create = function() {
